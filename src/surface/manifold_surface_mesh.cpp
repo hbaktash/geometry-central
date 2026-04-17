0862e8e8d2716a7b5f7204a7a6d5311473b56442
hbaktash <hbaktash@andrew.cmu.edu>
2026-04-17 19:31:44 -0400

    removed >=3 checks

--- src/surface/manifold_surface_mesh.cpp
+++ src/surface/manifold_surface_mesh.cpp
@@ -55,7 +55,7 @@ ManifoldSurfaceMesh::ManifoldSurfaceMesh(const std::vector<std::vector<size_t>>&
   nFacesCount = polygons.size();
   nVerticesCount = 0;
   for (const std::vector<size_t>& poly : polygons) {
-    GC_SAFETY_ASSERT(poly.size() >= 3, "faces must have degree >= 3");
+    // GC_SAFETY_ASSERT(poly.size() >= 3, "faces must have degree >= 3");
     for (auto i : poly) {
       nVerticesCount = std::max(nVerticesCount, i);
     }
